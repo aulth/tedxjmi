6c47e777dba0f9f51d20d2c3092ebeef930143c1
Arshad Jamal <codingspidy@gmail.com>
2023-08-30 10:50:08 +0530

    new intro mob

--- res/js/main.js
+++ res/js/main.js
@@ -292,3 +292,20 @@
     });
   });
 })(jQuery);
+
+const ticketElm = document.getElementById("ticket");
+    const ticketMob = document.getElementById("ticketMobile");
+    const { x, y, width, height } = ticketElm.getBoundingClientRect();
+    const centerPoint = { x: x + width / 2, y: y + height / 2 };
+    window.addEventListener("mousemove", (e) => {
+      const degreeX = (e.clientY - centerPoint.y) * 0.01;
+      const degreeY = (e.clientX - centerPoint.x) * -0.01;
+
+      ticketElm.style.transform = `perspective(1000px) rotateX(${degreeX}deg) rotateY(${degreeY}deg)`;
+    });
+    window.addEventListener("mousemove", (e) => {
+      const degreeX = (e.clientY - centerPoint.y) * 0.01;
+      const degreeY = (e.clientX - centerPoint.x) * -0.01;
+
+      ticketMob.style.transform = `perspective(1000px) rotateX(${degreeX}deg) rotateY(${degreeY}deg)`;
+    });
